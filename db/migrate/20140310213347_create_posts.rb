e105817f0363134997215c53f0d0684c24750805
Michael Martinez <zeroeclipse@live.com>
2021-03-28 18:58:30 -0700

    commit tree 78520d696e030339f2bacce2527ad8e001d324b2

--- db/migrate/20140310213347_create_posts.rb
+++ db/migrate/20140310213347_create_posts.rb
@@ -2,7 +2,9 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :name
-
+      t.text :content
+      t.integer :user_id
+      
       t.timestamps null: false
     end
   end
